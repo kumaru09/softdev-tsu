193adf8ca7c0fec9f7d51444d8431b9fd0796aaa
kumaru09 <ueki2542@gmail.com>
2020-04-23 00:59:09 +0700

    RegisterPage

---
+++ src/pages/Information.js
@@ -0,0 +1,139 @@
+import React, { Fragment } from 'react'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: '-1rem 0 2rem 0',
+        padding: '0 7rem',
+        [theme.breakpoints.down('xs')]: {
+            padding: '0'
+        },
+        [theme.breakpoints.down('md')]: {
+            padding: '0'
+        },
+        marginTop: 'auto'
+    },
+    textField: {
+        width: '100%',
+        [theme.breakpoints.down('xs')]: {
+            width: '100%'
+        }
+    },
+    errorMessage: {
+        color: 'red',
+        fontSize: '0.9rem',
+        marginTop: '0.2rem'
+    }
+}));
+
+
+const Information = ({ formProps: { register, errors }, handleInputChange, account }) => {
+    const classes = useStyles();
+    const { firstName, lastName, idCardNumber, phoneNumber, address } = account
+
+    return (
+        <Fragment>
+            <div className={classes.root}>
+                <Grid container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={0}
+                >
+                    <Grid item md={12} xs={12}>
+                        <TextField
+                            id="firstName"
+                            label="Firstname"
+                            name="firstName"
+                            className={classes.textField}
+                            margin="normal"
+                            variant="outlined"
+                            inputRef={register}
+                            error={!!errors.firstName}
+                            value={firstName}
+                            onChange={handleInputChange}
+                        />
+                        {errors.firstName && <p className={classes.errorMessage}>{errors.firstName.message}</p>}
+                    </Grid>
+                    
+                    <Grid item md={12} xs={12}>
+                        <TextField
+                            id="lastName"
+                            label="Lastname"
+                            name="lastName"
+                            className={classes.textField}
+                            margin="normal"
+                            variant="outlined"
+                            inputRef={register}
+                            error={!!errors.lastName}
+                            value={lastName}
+                            onChange={handleInputChange}
+                        />
+                        {errors.lastName && <p className={classes.errorMessage}>{errors.lastName.message}</p>}
+                    </Grid>
+                </Grid>
+
+                <Grid container
+                    direction="row"
+                    spacing={0} >
+                        <Grid item md={12} xs={12}>
+                            <TextField
+                                id="idCardNumber"
+                                label="Identification card number"
+                                name="idCardNumber"
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                inputRef={register}
+                                error={!!errors.idCardNumber}
+                                value={idCardNumber}
+                                onChange={handleInputChange}
+                            />
+                            {errors.idCardNumber && <p className={classes.errorMessage}>{errors.idCardNumber.message}</p>}
+                        </Grid>
+
+                        <Grid item md={12} xs={12}>
+                            <TextField
+                                id="phoneNumber"
+                                label="Phone number"
+                                name="phoneNumber"
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                inputRef={register}
+                                error={!!errors.phoneNumber}
+                                value={phoneNumber}
+                                onChange={handleInputChange}
+                            />
+                            {errors.phoneNumber && <p className={classes.errorMessage}>{errors.phoneNumber.message}</p>}
+                        </Grid>
+                </Grid>
+                    <Grid container
+                    direction="row"
+                    spacing={0} >
+                        <Grid item md={12} xs={12}>
+                            <TextField
+                                id="address"
+                                label="Address"
+                                name="address"
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                multiline
+                                rows={4}
+                                inputRef={register}
+                                error={!!errors.address}
+                                value={address}
+                                onChange={handleInputChange}
+                            />
+                            {errors.address && <p className={classes.errorMessage}>{errors.address.message}</p>}
+                        </Grid>
+                </Grid>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Information
---
+++ src/pages/StepperForm.js
@@ -0,0 +1,190 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepLabel from '@material-ui/core/StepLabel'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Information from './Information'
+import Account from './Account'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+    },
+    backButton: {
+        marginRight: theme.spacing(1),
+    },
+    instructions: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+    buttonLayout: {
+        marginLeft: '12rem',
+        [theme.breakpoints.down('xs')]: {
+            marginLeft: '0'
+        }
+    }
+}));
+
+const checkCharacterOnly = /[A-Za-z]+$/;
+const checkNumberOnly = /[0-9]+$/;
+
+const informationSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .required('This field is required.')
+        .matches(checkCharacterOnly, 'Please enter only characters.')
+        .min(3, 'Please enter at least 3 characters.'),
+
+    lastName: yup
+        .string()
+        .required('This field is required.')
+        .matches(checkCharacterOnly, 'Please enter only characters.')
+        .min(3, 'Please enter at least 3 characters.'),
+
+    idCardNumber: yup
+        .string()
+        .required('This field is required.')
+        .matches(checkNumberOnly, 'Please enter only number.')
+        .min(13, 'Please enter number 13 digits.')
+        .max(13, 'Please enter number 13 digits.'),
+    
+    phoneNumber: yup
+        .string()
+        .required('This field is required.')
+        .matches(checkNumberOnly, 'Please enter only number.')
+        .min(10, 'Please enter number 10 digits.')
+        .max(10, 'Please enter number 10 digits.'),
+
+    address: yup
+        .string()
+        .required('This field is required.')
+});
+
+const accountSchema = yup.object().shape({
+    email: yup
+        .string()
+        .email('Invalid email.')
+        .required('This field is required.'),
+    password: yup
+        .string()
+        .required('This field is required.')
+        .min(3, 'Please Enter less then 3 letters'),
+    confirmPassword: yup
+        .string()
+        .required('This field is required.')
+        .min(3, 'This field at least 3 characters.')
+        .oneOf([yup.ref('password'), null], "Password not match."),
+});
+
+function getSteps() {
+    return ['Information', 'Address'];
+}
+
+export default function StepperForm() {
+    const classes = useStyles();
+    const [account, setAccount] = useState({ 
+        firstName: "", 
+        lastName: "", 
+        idCardNumber: "",
+        phoneNumber: "",
+        address: "",
+        email: "", 
+        password: "", 
+        confirmPassword: ""
+    })
+
+    const informationForm = useForm({
+        validationSchema: informationSchema
+    });
+
+    const accountForm = useForm({
+        validationSchema: accountSchema
+    });
+
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    const onSubmit = data => {
+        handleNext()
+    };
+
+    function getStepContent(stepIndex) {
+        switch (stepIndex) {
+            case 0:
+                return <Information
+                            formProps={informationForm}
+                            handleInuputChange={handleInuputChange} 
+                            account={account}
+                        />;
+            case 1:
+                return <Account 
+                            formProps={accountForm} 
+                            handleInuputChange={handleInuputChange} 
+                            account={account}
+                        />;
+            default:
+                return 'Unknown stepIndex';
+        }
+    }
+
+    const handleInuputChange = event => {
+        const { name , value } = event.target
+
+        setAccount({ ...account, [name]: value })
+    }
+
+    return (
+        <div className={classes.root}>
+            <form onSubmit={activeStep === 0 ? informationForm.handleSubmit(onSubmit) : accountForm.handleSubmit(onSubmit)}>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map(label => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                <div>
+                    {activeStep === steps.length ? (
+                        <div className={classes.buttonLayout}>
+                            <Typography className={classes.instructions}>All steps completed</Typography>
+                            <Button variant="contained" color="primary" onClick={handleReset}>Reset</Button>
+                        </div>
+                    ) : (
+                            <div>
+                                <div className={classes.instructions}>{getStepContent(activeStep)}</div>
+                                <div className={classes.buttonLayout}>
+                                    <Button
+                                        disabled={activeStep === 0}
+                                        onClick={handleBack}
+                                        className={classes.backButton}
+                                        variant="outlined"
+                                    >
+                                        Back
+                                    </Button>
+                                    <Button variant="contained" color="primary" type="submit">
+                                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                    </Button>
+                                </div>
+                            </div>
+                        )}
+                </div>
+            </form>
+        </div>
+    );
+}
