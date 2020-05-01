1b4182e3f30c44fde9ac1117a52f0d4b958e0be6
kumaru09 <ueki2542@gmail.com>
2020-05-02 01:56:36 +0700

    Search

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,40 +1,101 @@
-import React, { useEffect } from "react";
-import CardTrip from "../component/CardTrip"
-import { Container, Typography, Grid, Paper } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import CardTrip from "../component/CardTrip";
+import {
+  Container,
+  Typography,
+  Grid,
+  Paper,
+  InputBase,
+  IconButton,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTours, toursSelector } from "../slices/tours";
+import { fetchTours, toursSelector, searchTours } from "../slices/tours";
 import { Tour } from "../component/Tour";
+import SearchIcon from "@material-ui/icons/Search";
 
+const useStyle = makeStyles((theme) => ({
+  iconButton: {
+    padding: 10,
+  },
+  header: {
+    position: "absolute",
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: 'cover',
+    width: '100%'
+  },
+  paper: {},
+}));
 
 const HomePage = () => {
-  const dispatch = useDispatch()
+  const classes = useStyle();
+  const dispatch = useDispatch();
+  const [input, setInput] = useState('')
 
   useEffect(() => {
-    dispatch(fetchTours())
+    dispatch(fetchTours());
+  }, [dispatch]);
 
-  }, [dispatch])
-
-  const { tours, loading, hasError } = useSelector(toursSelector) 
+  const { tours, loading, hasError } = useSelector(toursSelector);
 
   return (
-    <div>
+    <div
+      className={classes.header}
+      style={{ backgroundImage: "url(https://source.unsplash.com/random)" }}
+    >
+      {
+        <img
+          style={{ display: "none" }}
+          src="https://source.unsplash.com/random"
+          alt="..."
+        />
+      }
       <Container maxWidth="md">
-        <div style={{marginTop: "3rem"}}>
+        <Grid container justify="center">
+          <Grid item md={6}>
+            <Paper className={classes.paper}>
+              <Typography variant="subtitle1" gutterBottom>
+                ไปเที่ยวกัน
+              </Typography>
+              <IconButton
+                className={classes.iconButton}
+                aria-label="search"
+              >
+                <SearchIcon />
+              </IconButton>
+              <InputBase
+                className={classes.input}
+                placeholder="สถานที่"
+                inputProps={{ "aria-label": "ค้นหาสถานที่" }}
+                onChange={(event) => {setInput(event.target.value)}}
+              />
+              <Button color="primary" variant="contained" onClick={() => {dispatch(searchTours(input))}}>
+                ค้นหา
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+        <div style={{ marginTop: "3rem" }}>
           <Typography variant="h4" gutterBottom>
             {"ทริปแนะนำ"}
           </Typography>
           <Grid container spacing={3}>
-              <CardTrip />
-              <CardTrip />
-              <CardTrip />
+            <CardTrip />
+            <CardTrip />
+            <CardTrip />
           </Grid>
         </div>
-        <div style={{marginTop: "3rem"}}>
+        <div style={{ marginTop: "3rem" }}>
           <Typography variant="h4" gutterBottom>
             {"ทริปยอดนิยม"}
           </Typography>
           <Grid container spacing={1} direction="column">
-          {tours.map(tour => <Tour key={tour.id} tour={tour}  />)}
+            {tours.map((tour) => (
+              <Tour key={tour.id} tour={tour} />
+            ))}
           </Grid>
         </div>
       </Container>
--- src/slices/tours.js
+++ src/slices/tours.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import Axios from 'axios'
+import { history } from '../helpers/history'
 
 export const initialState = {
   loading: false,
@@ -34,9 +35,26 @@ export function fetchTours() {
   return async dispatch => {
     dispatch(getTours())
 
-    Axios.get('http://api.sdp.19991999.xyz/tours/',{search: ""})
+    Axios.get('http://api.sdp.19991999.xyz/tours/',{search: "1"})
     .then(res => {
         dispatch(getToursSuccess(res.data))
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch(getToursFailure())
+    })
+  }
+}
+
+export function searchTours(input) {
+  return async dispatch => {
+    dispatch(getTours())
+    console.log(input)
+    Axios.get('http://api.sdp.19991999.xyz/tours/',{search: 'test'})
+    .then(res => {
+        console.log(res.data)
+        dispatch(getToursSuccess(res.data))
+        history.push('/tours')
     })
     .catch(err => {
         console.log(err)
