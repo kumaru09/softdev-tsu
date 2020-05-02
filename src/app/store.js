3c519f963b59e3c1c6f5758419c01d592143895d
kumaru09 <ueki2542@gmail.com>
2020-05-03 01:04:49 +0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from '../slices/login'
 import tourReducer from '../slices/tour'
 import toursReducer from '../slices/tours';
+import registerReducer from '../slices/register';
 
 export default configureStore({
   reducer: {
     login: loginReducer,
     tour: tourReducer,
-    tours: toursReducer
+    tours: toursReducer,
+    register: registerReducer
   },
 });
