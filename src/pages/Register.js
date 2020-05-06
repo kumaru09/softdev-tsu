4bbd47cdee568ddc8c6181e87473a22e765d1972
Weeraton Libisittikul <niceand2556@gmail.com>
2020-05-06 15:37:02 +0700

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -64,12 +64,14 @@ const registerSchema = yup.object().shape({
     .required("This field is required.")
     .matches(checkCharacterOnly, "Please enter only characters.")
     .min(3, "Please enter at least 3 characters."),
+	.max(50, "Please enter at less than 50 characters."),
 
   lastName: yup
     .string()
     .required("This field is required.")
     .matches(checkCharacterOnly, "Please enter only characters.")
     .min(3, "Please enter at least 3 characters."),
+	.max(50, "Please enter at less than 50 characters."),
 
   phoneNumber: yup
     .string()
@@ -91,11 +93,13 @@ const registerSchema = yup.object().shape({
     .string()
     .required("This field is required.")
     .min(3, "Please Enter less then 3 letters"),
-
+	.max(20, "Please enter at less than 20 characters."),
+	
   confirmPassword: yup
     .string()
     .required("This field is required.")
     .min(3, "This field at least 3 characters.")
+	.max(20, "Please enter at less than 20 characters."),
     .oneOf([yup.ref("password"), null], "Password not match."),
 });
 
