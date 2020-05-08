1a795d7415e552be4a4226b5150c7e522dfb3532
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-05-08 09:40:53 +0700

    show picture in tour list

--- src/component/Tour.js
+++ src/component/Tour.js
@@ -1,21 +1,40 @@
 import React, { Fragment } from 'react'
-import { Typography, ListItem, ListItemText, Divider } from '@material-ui/core'
+import { Typography, ListItem, ListItemText, Divider, Grid, CardMedia, makeStyles } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
-export const Tour = ({ tour }) => (
-  <Fragment>
-    <ListItem component={Link} to={`/tours/${tour.id}`} style={{ color: '#000' }}>
-      <ListItemText
-        primary={<Typography component="span" variant="h6">{tour.name}</Typography>}
-        secondary={
-          <Fragment>
-            <Typography component="span" variant="body1">
-              {tour.description.substring(0, 250)}
-            </Typography>
-            {" - " + tour.g_name}
-          </Fragment>}
-      />
-    </ListItem>
-    <Divider variant="fullWidth" />
-  </Fragment>
-)
+const useStyles = makeStyles((theme) => ({
+  media: {
+    padding: "80px",
+  }
+}));
+
+export const Tour = ({ tour }) => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <ListItem component={Link} to={`/tours/${tour.id}`} style={{ color: '#000' }}>
+        <ListItemText
+          primary={<Typography component="span" variant="h6">{tour.name}</Typography>}
+          secondary={
+            <Fragment>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={4}>
+                  <CardMedia
+                    className={classes.media}
+                    image={`https://api.19991999.xyz/pic/${tour.pic}`}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                  <Typography component="span" variant="body1">
+                    {tour.description.substring(0, 250)}
+                  </Typography>
+                  {" - " + tour.g_name}
+                </Grid>
+              </Grid>
+            </Fragment>}
+        />
+      </ListItem>
+      <Divider variant="fullWidth" />
+    </Fragment>
+  )
+}
